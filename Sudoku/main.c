8bf20974f19ccd142a05a57886cee64ef40bb909
jams <j_a_mosquera@hotmail.com>
2020-02-02 14:14:22 -0500

    Initial C file

---
+++ Sudoku/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: j_a_m
+ *
+ * Created on February 2, 2020, 2:09 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    return (0);
+}
+
